550c46101028b7677323c94d308b703b571727c4
gox <gox.roche@gmail.com>
2020-01-03 19:02:21 +0100

    Manage RT and quoted tweets

--- twitter.js
+++ twitter.js
@@ -29,18 +29,28 @@ let oembedUpdate = () => {
     });
 }
 
-let processTweet = (tweet) => {
+let buildTweetUrl = (tweet) => {
+    return "https://twitter.com/" + tweet.user.screen_name + "/status/" + tweet.id_str;
+}
 
-    const url = "https://twitter.com/" + tweet.user.screen_name + "/status/" + tweet.id_str;
+let processTweet = (t) => {
+    let tweet = t;
+    let url = buildTweetUrl(tweet);
 
-    if (tweet.retweeted_status || tweet.in_reply_to_status_id || tweet.in_reply_to_status_id_str 
-        || tweet.in_reply_to_user_id || tweet.in_reply_to_user_id_str || tweet.in_reply_to_screen_name) {
+    if (tweet.retweeted_status) {
+        tweet = tweet.retweeted_status;
+        url = buildTweetUrl(tweet);
+        logger.info(url + " is a RT");
+    }
+    else if(tweet.in_reply_to_status_id || tweet.in_reply_to_status_id_str){
+        url = buildTweetUrl(tweet);
         logger.info(url + " is a response");
-        return;
+        return; // ignore it at the moment
     }
     else if (tweet.id_str && tweet.quoted_status && tweet.id_str != tweet.quoted_status) {
+        tweet = tweet.quoted_status;
+        url = buildTweetUrl(tweet);
         logger.info(url + " is a quote");
-        return;
     }
     else {
         logger.info(url);
@@ -60,7 +70,7 @@ let processTweet = (tweet) => {
     });
 }
 
-let processError = (error) => {
+let processError = (err) => {
     logger.error("processError " + JSON.stringify(err));
 }
 
